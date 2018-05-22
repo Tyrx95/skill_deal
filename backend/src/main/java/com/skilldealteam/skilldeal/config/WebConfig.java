efb476b49ba03ea54547728013ea1d9dfe709552
Tyrx95 <amartiric@gmail.com>
2018-05-22 12:09:19 +0200

    CORS issue try 6

--- backend/src/main/java/com/skilldealteam/skilldeal/config/WebConfig.java
+++ backend/src/main/java/com/skilldealteam/skilldeal/config/WebConfig.java
@@ -1,6 +1,11 @@
 package com.skilldealteam.skilldeal.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -10,9 +15,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    /*
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        /*registry.addMapping("/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost",
                                 "http://127.0.0.1",
                                 "http://skilldeal.bss.design/",
@@ -26,8 +32,24 @@ public class WebConfig extends WebMvcConfigurerAdapter {
                                 "https://31.13.112.129:*"
 
                 )
-                .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");*/
+                .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
         registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedOrigins("*")
                 .allowedHeaders("*");
+    }*/
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
+
+
 }
